9c28fc0bcb2addc505ca480667129b0cf03ec4e5
Sonali372 <sonali.rai@sunstone.in>
2025-07-01 16:56:13 +0530

    Create Reverse_String.js

---
+++ Reverse_String.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString("JavaScript"));
+console.log(reverseString("OpenAI"));
+console.log(reverseString("Palindrome"));
+console.log(reverseString("Prerna"));
+console.log(reverseString("Sonali"));
+console.log(reverseString("Prince"));
+console.log(reverseString("Sunstone"));
+
+
